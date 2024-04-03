d3edb70d1fccfdcefa50ab3162bf9d9ba7a4ef8c
Thiago C H Moreira <thiagochm@gmail.com>
2024-04-03 11:38:35 -0300

    Instalando Swagger

--- src/server.ts
+++ src/server.ts
@@ -11,11 +11,12 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
-import { errorHandler } from "./error-handler";
+import { errorHandler } from "../error-handler";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
+  // origin: 'http://minhaURL.com',
   origin: '*',
 })
 
